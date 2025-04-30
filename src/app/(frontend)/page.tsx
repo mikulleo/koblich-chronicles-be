6a46243e2f11fa620b83a165007d2d8bd79fe2da
Leos Mikulka <mikulkal@hotmail.com>
2025-04-30 20:20:57 +0200

    Updated config + dockerfile

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,7 @@
 import PageTemplate, { generateMetadata } from './[slug]/page'
 
-export default PageTemplate
+export default async function HomePage() {
+  return <PageTemplate params={{ slug: 'home' }} />
+}
 
-export { generateMetadata }
+export { generateMetadata } from './[slug]/page'
